c975e1c4848ccebee63f6641414f8e3a1d24222a
Sergey Berezhnoy <veged@yandex-team.ru>
2016-06-16 17:34:08 +0300

    - onInit - mods - attrs - _buildClassName

--- index.js
+++ index.js
@@ -3,19 +3,43 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import inherit from 'inherit';
+import b from 'b_'; // TODO: optimize
 
 function BEM() {
 
 }
 
 var BaseComponent = inherit(Component, {
+    __constructor() {
+        this.__base.apply(this, arguments);
+        this.onInit(this.props);
+    },
+
+    onInit() {},
+
     tag() {
         return 'div';
     },
 
+    attrs() {
+        return null;
+    },
+
+    mods() {
+        return null;
+    },
+
+    _buildClassName() {
+        return b(this.block, this.elem, this.mods(this.props));
+    },
+
     render() {
         var Tag = this.tag(this.props);
-        return <Tag>{ this.content(this.props, this.props.children) }</Tag>;
+        return (
+            <Tag className={this._buildClassName()} {...this.attrs(this.props)}>
+                { this.content(this.props, this.props.children) }
+            </Tag>
+        );
     },
 
     content(_, children) {
@@ -24,15 +48,18 @@ var BaseComponent = inherit(Component, {
 });
 
 function wrapWithFunction(obj, name) {
-    if(obj.hasOwnProperty(name)) {
-        var val = obj[name];
-        typeof val !== 'function' && (obj[name] = () => val);
+    if(Array.isArray(name)) {
+        name.forEach(n => wrapWithFunction(obj, n));
+    } else {
+        if(obj.hasOwnProperty(name)) {
+            var val = obj[name];
+            typeof val !== 'function' && (obj[name] = () => val);
+        }
     }
 }
 
 BEM.decl = (fields, staticFields) => {
-    wrapWithFunction(fields, 'tag');
-    wrapWithFunction(fields, 'content');
+    wrapWithFunction(fields, ['tag', 'attrs', 'content', 'mods']);
 
     return inherit(BaseComponent, fields, staticFields);
 };
@@ -42,14 +69,49 @@ BEM.decl = (fields, staticFields) => {
 
 var MyBlock = BEM.decl({
     block : 'MyBlock',
-    tag : 'a'
+    mods({ disabled }) {
+        return {
+            disabled,
+            a : true,
+            b : 1
+        };
+    },
+    tag : 'a',
+    attrs : { href : '//yandex.ru' }
+});
+
+// OtherBlock.js
+
+var OtherBlock = BEM.decl({
+    block : 'OtherBlock',
+    tag : 'input',
+    attrs({ value, onChange }) {
+        return {
+            value,
+            onChange
+        };
+    }
+});
+
+// Root.js
+
+var Root = BEM.decl({
+    block : 'Root',
+    onInit() {
+        this.state = { value : '567' };
+    },
+    content() {
+        return [
+            <MyBlock key="1"/>,
+            <MyBlock key="2" disabled>321</MyBlock>,
+            <OtherBlock
+                key="3"
+                value={this.state.value}
+                onChange={({ target }) => this.setState({ value : target.value }) }/>
+        ];
+    }
 });
 
 // index.js
 
-ReactDom.render(
-    <div>
-        <MyBlock/>
-        <MyBlock>321</MyBlock>
-    </div>,
-    document.getElementById('root'));
+ReactDom.render(<Root/>, document.getElementById('root'));
