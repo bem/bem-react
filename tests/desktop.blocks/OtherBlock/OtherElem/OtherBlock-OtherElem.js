99b7f0ff51d2a79627237c4e29e2916c70f91006
Sergey Berezhnoy <veged@ya.ru>
2016-10-20 23:15:35 +0300

    Update OtherBlock-OtherElem.js
    code style

--- tests/desktop.blocks/OtherBlock/OtherElem/OtherBlock-OtherElem.js
+++ tests/desktop.blocks/OtherBlock/OtherElem/OtherBlock-OtherElem.js
@@ -3,9 +3,9 @@ import MyBlock from 'b:MyBlock';
 import MyElem from 'b:MyBlock e:MyElem';
 
 export default decl({
-  block: 'OtherBlock',
-  elem: 'OtherElem',
-  content() {
-    return <MyElem />;
-  }
+    block: 'OtherBlock',
+    elem: 'OtherElem',
+    content() {
+      return <MyElem />;
+    }
 });
