8bfc4ed99a1caabf4ceebbda4eee2cd9b38b8767
Sergey Berezhnoy <veged@yandex-team.ru>
2016-10-21 15:04:32 +0300

    Introduce `wrap` method (closes #18)

---
+++ tests/common.blocks/WrappedBlock/WrappedBlock.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Bem, {decl} from 'bem-react-core';
+
+export default decl({
+    block : 'WrappedBlock',
+
+    wrap(wrappedBlock) {
+        return <Bem block="Wrapper">{wrappedBlock}</Bem>;
+    }
+});
