49133d54713732df2eb26e6b3ef3dc86dae169f3
awinogradov <winogradovaa@gmail.com>
2017-05-19 16:40:02 +0300

    improve ci

---
+++ .bemrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    levels : {
+        './tests/fixtures1.blocks' : {
+            scheme : 'nested'
+        },
+        './tests/fixtures2.blocks' : {
+            scheme : 'nested'
+        },
+        './tests/fixtures3.blocks' : {
+            scheme : 'flat',
+            naming : 'origin'
+        }
+    }
+};
