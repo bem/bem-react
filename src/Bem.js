b53784139c8b9698184f6880e8e4932a411d3d5b
awinogradov <winogradovaa@gmail.com>
2017-10-19 23:21:56 +0300

    inject className stringifier

--- src/Bem.js
+++ src/Bem.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
 import inherit from 'inherit';
+import stringify from '@bem/sdk.naming.entity.stringify';
+import presets from '@bem/sdk.naming.presets';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import CNB from './ClassNameBuilder';
 
 export default inherit(Component, {
     __constructor() {
         this.__base(...arguments);
-        this.__cnb || (this.__cnb = this.__self.cnb());
-    },
-
-    __render(node) {
-        return React.createElement(node.tag || 'div', {
-            className : this.__cnb.str(node),
-            ...node.attrs
-        }, node.children);
+        this.__cnb || (this.__cnb = this.__self.__naming());
     },
 
     getChildContext() {
@@ -44,13 +38,16 @@ export default inherit(Component, {
         if(!node.block) throw Error('Can\'t get block from context');
 
         return this.__render(node);
+    },
+
+    __render(node) {
+        return React.createElement(node.tag || 'div', {
+            className : this.__cnb(node),
+            ...node.attrs
+        }, node.children);
     }
 }, {
     displayName : 'Bem',
-
-    cnb() {
-        return new CNB(this.__dangerouslySetNaming);
-    },
 
     childContextTypes : {
         bemBlock : PropTypes.string
@@ -62,8 +59,53 @@ export default inherit(Component, {
 
     __dangerouslySetNaming : 'react',
 
+    __naming() {
+        const str = stringify(presets[this.__dangerouslySetNaming]);
+        return ({ addBemClassName = true, block, elem, mods, mix, cls }) => {
+            if(addBemClassName) {
+                const entities = [{ block, elem }];
+                mods && Object.keys(mods).forEach(name => {
+                    const val = mods[name];
+                    val && entities.push({ block, elem, mod : { name, val } });
+                });
+
+                if(mix) {
+                    const mixedEntities = [].concat(...mix).reduce((uniq, mixed) => {
+                        if(!mixed) return uniq;
+
+                        const k = `${mixed.block}$${mixed.elem}`;
+
+                        if(uniq[k]) uniq[k].mods = Object.assign({}, uniq[k].mods, mixed.mods);
+                        else uniq[k] = mixed;
+
+                        return uniq;
+                    }, {});
+
+                    Object.keys(mixedEntities).forEach(k => {
+                        const mixed = mixedEntities[k],
+                            mixedMods = mixed.mods,
+                            mixedBlock = mixed.block || block,
+                            mixedElem = mixed.elem;
+
+                        entities.push({ block : mixedBlock, elem : mixedElem });
+
+                        mixedMods && Object.keys(mixedMods).forEach(name => {
+                            const val = mixedMods[name];
+                            val && entities.push({ block : mixedBlock, elem : mixedElem, mod : { name, val } });
+                        });
+                    });
+                }
+
+                cls && entities.push(cls);
+
+                return entities.map(entity => typeof entity === 'string'?
+                    entity : str(entity)).join(' ');
+            }
+        };
+    },
+
     __displayName(block, elem) {
-        this.__cnb || (this.__cnb = this.cnb());
-        return this.__cnb.str({ addBemClassName : true, block, elem });
+        this.__cnb || (this.__cnb = this.__naming());
+        return this.__cnb({ block, elem });
     }
 });
