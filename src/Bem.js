6d814cedd04324734ecd0b9f87595343a0a1d020
awinogradov <winogradovaa@gmail.com>
2017-10-19 18:52:21 +0300

    renderTag inside Base

--- src/Bem.js
+++ src/Bem.js
@@ -4,33 +4,24 @@ import Base from './Base';
 export default function(overrideFields={}, overrideStaticFields={}) {
     return inherit(Base, {
         render() {
-            let {
-                addBemClassName = true,
-                block,
-                elem,
-                mods,
-                tag : Tag = 'div',
-                mix,
-                attrs,
-                cls,
-                children
-            } = this.props;
+            let node = Object.assign({}, this.props);
+            const { bemBlock } = this.context;
 
-            if(!elem && !block && this.context.bemBlock) throw Error('Prop elem must be specified');
+            if(!node.elem && !node.block && bemBlock) throw Error('Prop elem must be specified');
 
-            const typeOfBlock = typeof block;
+            const typeOfBlock = typeof node.block;
             if(typeOfBlock === 'undefined')
-                block = this.context.bemBlock;
+                node.block = bemBlock;
             /* istanbul ignore next */
             else if(typeOfBlock === 'object')
-                block = block.block;
+                node.block = block.block;
             /* istanbul ignore next */
             else if(typeOfBlock === 'function')
-                block = block.prototype.block;
+                node.block = block.prototype.block;
 
-            if(!block) throw Error('Can\'t get block from context');
+            if(!node.block) throw Error('Can\'t get block from context');
 
-            return this.__render(addBemClassName, Tag, attrs, block, elem, mods, mix, cls, children);
+            return this.__render(node);
         },
 
         ...overrideFields
