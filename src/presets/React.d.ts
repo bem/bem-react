a21fa9e83e67205ea90d2eeed324cab78560584a
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-12-04 13:05:57 +0300

    WIP: d.ts

---
+++ src/presets/React.d.ts
@@ -0,0 +1,14 @@
+
+export as namespace bemReactCore;
+
+// bemReactCoreClass
+// dynamicFieldsInterface
+// staticFieldsInterface
+
+// object with applyDecls as result of decl
+
+export function decl(base?: class | Array<class>, dynamicFields: object, staticFields?: object, wrapper?: function): object;
+// predicate
+export function declMod(): object;
+
+export BEM
