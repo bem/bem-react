8a1910363fe80329455575736050402166c0f0a2
awinogradov <winogradovaa@gmail.com>
2017-12-16 02:38:17 +0300

    more types

--- src/presets/React.d.ts
+++ src/presets/React.d.ts
@@ -1,14 +1,59 @@
+declare module 'bem-react-core' {
+    import React from 'react';
 
-export as namespace bemReactCore;
+    type Props = React.ClassAttributes;
+    type Klass = React.ComponentClass;
+    type ObjectMode = ((props: Props, state: object) => object) | object;
+    type Content = null | string | number | JSX.Element;
+    type MultipleContent = Array<Content>;
 
-// bemReactCoreClass
-// dynamicFieldsInterface
-// staticFieldsInterface
+    interface Declaration {
+        applyDecls(): Klass;
+    }
 
-// object with applyDecls as result of decl
+    interface Mods {
+        [modName]: boolean | string;
+    }
 
-export function decl(base?: class | Array<class>, dynamicFields: object, staticFields?: object, wrapper?: function): object;
-// predicate
-export function declMod(): object;
+    interface JsonMix {
+        block: string;
+        elem?: string;
+        mods?: Mods;
+        elemMods?: Mods;
+    }
 
-export BEM
+    type Mix = JsonMix | string | JSX.Element;
+    type Replaceble = null | number | string | JSX.Element;
+
+    interface Block {
+        block: string;
+        tag?: string;
+        cls?: ((props: Props, state: object) => string) | string;
+        addBemClassName?: ((props: Props, state: object) => boolean) | boolean;
+        style?: ObjectMode;
+        attrs?: ObjectMode;
+        mods?: Mods | ((props: Props, state: object) => Mods);
+        mix?: Mix | ((props: Props, state: object) => Mix);
+        addMix?: Mix | ((props: Props, state: object) => Mix);
+        content?: ((props: Props, state: object) => Content | MultipleContent) | Content | MultipleContent;
+        replace?: ((props: Props, state: object) => Replaceble) | Replaceble;
+        wrap?(props: Props, state: object, klass: Klass): Content;
+        willInit?(props: Props): void;
+    }
+
+    interface Elem extends Block {
+        elem: string;
+    }
+
+    type Mixin = Klass | Array<Klass>;
+    type Entity = Block|Elem;
+
+    interface EntityStatic {
+        propTypes?: Props,
+        contextTypes?: Props,
+        childContextTypes?: Props,
+        [fieldName]: any
+    }
+
+    export function decl(/*mixin?: Mixin, */entity: Entity, static?: EntityStatic): Declaration;
+}
