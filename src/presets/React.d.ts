a44f803874ea052d4b9770012f317193a722e069
Konstantin Gladkikh <birhoff@gmail.com>
2017-12-16 17:12:58 +0300

    docs(d.ts): Add core declarations

--- src/presets/React.d.ts
+++ src/presets/React.d.ts
@@ -2,17 +2,17 @@ declare module 'bem-react-core' {
     import React from 'react';
 
     type Props = React.ClassAttributes;
-    type Klass = React.ComponentClass;
+    type ReactClass = React.ComponentClass;
     type ObjectMode = ((props: Props, state: object) => object) | object;
     type Content = null | string | number | JSX.Element;
     type MultipleContent = Array<Content>;
 
-    interface Declaration {
-        applyDecls(): Klass;
+    interface Declaration<P> {
+        applyDecls(): React.ComponentClass<P>;
     }
 
     interface Mods {
-        [modName]: boolean | string;
+        [modName: string]: boolean | string;
     }
 
     interface JsonMix {
@@ -37,7 +37,7 @@ declare module 'bem-react-core' {
         addMix?: Mix | ((props: Props, state: object) => Mix);
         content?: ((props: Props, state: object) => Content | MultipleContent) | Content | MultipleContent;
         replace?: ((props: Props, state: object) => Replaceble) | Replaceble;
-        wrap?(props: Props, state: object, klass: Klass): Content;
+        wrap?(props: Props, state: object, klass: ReactClass): Content;
         willInit?(props: Props): void;
     }
 
@@ -45,16 +45,64 @@ declare module 'bem-react-core' {
         elem: string;
     }
 
-    type Mixin = Klass | Array<Klass>;
+    type Mixin = ReactClass | Array<ReactClass>;
     type Entity = Block|Elem;
 
     interface EntityStatic {
         propTypes?: Props,
         contextTypes?: Props,
         childContextTypes?: Props,
-        [fieldName]: any
+        [fieldName: string]: any
     }
 
-    export function decl(entity: Entity, static?: EntityStatic): Declaration;
-    export function decl(mixin?: Mixin, entity: Entity, static?: EntityStatic): Declaration;
+    /**
+     * Базовые bem свойства в объеденинии с поддерживаемыми HTML свойствами.
+     */
+    interface BemBlock extends React.HTMLProps {
+        block?: string;
+        elem?: string
+        tag?: string;
+        cls?: string;
+        addBemClassName?: boolean;
+        style?: object;
+        attrs?: object;
+        mods?: Mods;
+        mix?: Mix;
+    }
+
+    /**
+     * Базовый компонент, чтоб не делать декларации для простых блоков
+     */
+    class Bem<S> extends React.Component<BemBlock, S> {
+        props: Readonly<BemBlock>;
+    }
+
+    /**
+     * Матчер на модификатор
+     *
+     * @description
+     * Обычно это функция, но есть короткий вариант [сахар]{@link PredicateShort}
+     */
+    type Predicate = ((props: Props, state: object) => boolean | PredicateShort)
+
+    /**
+     * Сахар над матчером на модификаторб передается в ввиде хеша
+     *
+     * @description
+     * Значения могут быть 3х типов
+     *  - функция - вызовится для проверки
+     *  - значение - значение с простым типом данных
+     *  - '*' - специальное значение, матчит на присутвие свойства
+     */
+    interface PredicateShort {
+        [prop: string]: ((props: Props, state: object) => boolean) | (number | string | boolean) | '*'
+    }
+
+    export function decl(entity: Entity, static?: EntityStatic, wrapper?: Function): Declaration;
+    export function decl(mixin: Mixin, entity: Entity, static?: EntityStatic, wrapper?: Function): Declaration;
+
+    /**
+     * Декларация модификатора
+     */
+    export function declMod(predicate: Predicate, fields: Entity, staticFields?: EntityStatic): Declaration;
 }
