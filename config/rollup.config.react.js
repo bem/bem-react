20a5825cefe7aab3cdeeb05d2f232e7c35629876
awinogradov <winogradovaa@gmail.com>
2017-12-18 18:16:57 +0300

    better build

--- config/rollup.config.react.js
+++ config/rollup.config.react.js
@@ -2,29 +2,29 @@ import replace from 'rollup-plugin-replace';
 import { config, output } from './rollup.config.common';
 
 export default [{
-    ...output,
-    format : 'cjs',
-    sourcemap : true,
-    file : 'dist/index.react.dev.js'
+    ...config,
+    output : {
+        ...output,
+        format : 'cjs',
+        sourcemap : true,
+        file : 'dist/react.dev.js'
+    }
 }, {
-    ...output,
-    format : 'umd',
-    globals : {
-        react : 'React',
-        'prop-types' : 'PropTypes'
+    ...config,
+    output : {
+        ...output,
+        format : 'umd',
+        globals : {
+            react : 'React',
+            'prop-types' : 'PropTypes'
+        },
+        file : 'umd/react.js'
     },
-    file : 'umd/index.react.js'
-}].map(output => {
-    const res = {
-        ...config,
-        input : 'src/presets/React.js',
-        external : ['react', 'prop-types'],
-        output
-    };
-
-    output.format === 'umd' && res.plugins.push(replace({
+    plugins : [].concat(config.plugins, replace({
         'process.env.BEM_NAMING' : 0
-    }));
-
-    return res;
-});
+    }))
+}].map(c => ({
+    ...c,
+    input : 'src/presets/React.js',
+    external : ['react', 'prop-types']
+}));
