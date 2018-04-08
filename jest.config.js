ff3edc11f13c070528b756aad398edeadc16a052
awinogradov <winogradovaa@gmail.com>
2018-04-09 00:05:52 +0300

    refactor: prepare configs for TS

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    setupFiles : ['./tests/setup.js'],
-    testPathIgnorePatterns : ['./examples', 'node_modules'],
-    moduleNameMapper : {
-        'bem-react-core' : process.env.CI ? '<rootDir>' : `<rootDir>/src/presets/${process.env.PRESET || 'React'}`
-    }
-};
