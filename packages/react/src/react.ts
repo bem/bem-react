93adf0ef95bdf85353ac69a173a4e52b7cf920a2
yarastqt <yarastqt@gmail.com>
2018-04-11 17:22:49 +0300

    feat(index): use Component instead PureComponent

--- packages/react/src/react.ts
+++ packages/react/src/react.ts
@@ -1,12 +1,10 @@
 import { react } from '@bem/sdk.naming.presets';
-import * as React from 'react';
+import { Component, createElement } from 'react';
 
 import { declareBemCore } from './';
 
-const { Bem, Block, Elem, mod } = declareBemCore({
+export const { Bem, Block, Elem, mod } = declareBemCore({
     naming: react,
-    Base: React.PureComponent,
-    render: React.createElement
+    Base: Component,
+    render: createElement
 });
-
-export { Bem, Block, Elem, mod };
