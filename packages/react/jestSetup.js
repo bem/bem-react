c51c2af3325d9e1fea3c9a9260118399dc3fb9d3
yarastqt <yarastqt@gmail.com>
2018-06-24 01:28:50 +0300

    chore: add env for test

--- packages/react/jestSetup.js
+++ packages/react/jestSetup.js
@@ -1,6 +1,8 @@
 const { configure } = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
+const { NODE_ENV } = process.env;
+
 configure({ adapter: new Adapter() });
 
-global.__DEV__ = true;
+global.__DEV__ = NODE_ENV === 'test';
