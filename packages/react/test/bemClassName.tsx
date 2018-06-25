dae65203aef11d20f67f597b273d89bf16111261
yarastqt <yarastqt@gmail.com>
2018-06-25 19:20:24 +0300

    feat: add bem class name helper

---
+++ packages/react/test/bemClassName.tsx
@@ -0,0 +1,40 @@
+import { origin, react } from '@bem/sdk.naming.presets';
+import { mount } from 'enzyme';
+import * as React from 'react';
+
+import { bemClassName, Block, Elem } from '../src';
+
+describe('bemClassName:', () => {
+    it('should return classname', () => {
+        class MyBlock extends Block {
+            public content() {
+                return (
+                    <div className={this.bemClassName('MyElem')} />
+                );
+            }
+        }
+
+        class MyElem extends Elem {
+            public block = 'MyBlock';
+            public elem = 'MyElem';
+            public content() {
+                return (
+                    <div className={this.bemClassName({ theme: 'normal' })} />
+                );
+            }
+        }
+
+        class MyBlock1 extends Block {
+            public block = 'myblock1';
+            public content() {
+                return (
+                    <div className={bemClassName(origin)('myblock2', 'myelem2')} />
+                );
+            }
+        }
+
+        expect(mount(<MyBlock />).find('.MyBlock-MyElem')).toHaveLength(1);
+        expect(mount(<MyElem />).find('.MyBlock-MyElem_theme_normal')).toHaveLength(1);
+        expect(mount(<MyBlock1 />).find('.myblock2__myelem2')).toHaveLength(1);
+    });
+});
