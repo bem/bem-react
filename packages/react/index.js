d652328a8202e6df2d54d0847bd44d53bee39d54
awinogradov <winogradovaa@gmail.com>
2018-08-29 00:37:33 +0300

    feat(v3): init packages

--- packages/react/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./build/cjs/bem-react-core.production.min.js');
-} else {
-    module.exports = require('./build/cjs/bem-react-core.development.js');
-}
---
+++ scripts/rollup/config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const BUNDLE_TYPES = {
+    NODE_DEV: {
+        minify: false,
+        format: 'cjs',
+        asserts: true
+    },
+    NODE_PROD: {
+        minify: true,
+        format: 'cjs',
+        asserts: false
+    }
+};
+
+const bundleConfig = {
+    name: process.env.PKG,
+    entry: `./packages/${process.env.PKG}/${process.env.PKG}.tsx`,
+    typings: 'typings',
+    bundleTypes: [
+        BUNDLE_TYPES.NODE_DEV,
+        BUNDLE_TYPES.NODE_PROD
+    ],
+    externals: [
+        'object-assign',
+        'react',
+    ]
+};
+
+module.exports.BUNDLE_TYPES = BUNDLE_TYPES;
+module.exports.bundleConfig = bundleConfig;
