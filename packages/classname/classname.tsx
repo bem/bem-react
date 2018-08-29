b2a19566704bfa78d144ed02db197952e4c90c8f
awinogradov <winogradovaa@gmail.com>
2018-08-29 13:37:44 +0300

    chore(classname): rename main formatter

--- packages/classname/classname.tsx
+++ packages/classname/classname.tsx
@@ -27,7 +27,7 @@ function modsToArray(block: string, elem?: string, mods?: NoStrictEntityMods) {
     return arr;
 }
 
-export function entity(block: string, elem?: string): EntityFormatter {
+export function cn(block: string, elem?: string): EntityFormatter {
     const naming = stringifyWrapper(react);
 
     return (mods?: NoStrictEntityMods) => modsToArray(block, elem, mods).map(naming).join(' ');
