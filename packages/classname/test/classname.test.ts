fe78789a5c9690bf4c49f8a7dbdbbf7595d97899
awinogradov <winogradovaa@gmail.com>
2018-08-30 00:31:19 +0300

    test(classname): full coverage

--- packages/classname/test/classname.test.ts
+++ packages/classname/test/classname.test.ts
@@ -35,6 +35,21 @@ describe('cn: default', () => {
             expect(b(mods)).to.be.eq('Block_modName Block_modName2_modVal');
             expect(e(mods)).to.be.eq('Block-Elem_modName Block-Elem_modName2_modVal');
         });
+
+        it('empty modifiers', () => {
+            const b = cn('Block');
+            expect(b({})).to.be.eq('Block');
+        });
+
+        it('falsy modifiers', () => {
+            const b = cn('Block');
+            expect(b({ modName: false })).to.be.eq('Block');
+        });
+
+        it('zero', () => {
+            const b = cn('Block');
+            expect(b({ modName: 0 })).to.be.eq('Block_modName_0');
+        });
     });
 });
 
@@ -69,6 +84,21 @@ describe('cn: configurable', () => {
 
             expect(b(mods)).to.be.eq('block_modName block_modName2_modVal');
             expect(e(mods)).to.be.eq('block__elem_modName block__elem_modName2_modVal');
+        });
+
+        it('empty modifiers', () => {
+            const b = cCn('block');
+            expect(b({})).to.be.eq('block');
+        });
+
+        it('falsy modifiers', () => {
+            const b = cCn('block');
+            expect(b({ modName: false })).to.be.eq('block');
+        });
+
+        it('zero', () => {
+            const b = cCn('block');
+            expect(b({ modName: 0 })).to.be.eq('block_modName_0');
         });
     });
 });
