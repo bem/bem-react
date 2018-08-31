cdff5240c5fdf4666173889b9bde41cac1fddcd7
awinogradov <winogradovaa@gmail.com>
2018-08-31 19:07:18 +0300

    docs: api reference by typedoc

---
+++ typedoc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'BEM React: API Reference',
+    theme: 'minimal',
+    out: './reference',
+    readme: './docs/ru/README.md',
+    mode: 'file',
+    exclude: '**/node_modules',
+    ignoreCompilerErrors: true,
+    excludeNotExported: true,
+    hideGenerator: true
+};
