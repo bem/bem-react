ad2e5ff2314defc7cb51c5850cff2ab7d9fca4e5
yarastqt <yarastqt@gmail.com>
2018-10-01 14:01:14 +0300

    test(di): add tests for registry container

---
+++ packages/di/test/di.test.tsx
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+
+import { Registry } from '../di';
+
+describe('@bem-react/di', () => {
+    describe('Registry', () => {
+        it('should set components and return this by id', () => {
+            const registry = new Registry({ id: 'id' });
+            const Component1 = () => null;
+            const Component2 = () => null;
+
+            registry.set('id-1', Component1);
+            registry.set('id-2', Component2);
+
+            expect(registry.get('id-1')).to.equal(Component1);
+            expect(registry.get('id-2')).to.equal(Component2);
+        });
+    });
+
+    describe.skip('withRegistry', () => {
+        // TODO: Add test for withRegistry
+        return null;
+    });
+});
