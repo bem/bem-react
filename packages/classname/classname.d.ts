c17da64b20150fb4c8e08cd57d145cbd818e482a
awinogradov <winogradovaa@gmail.com>
2018-11-26 15:51:43 +0300

    chore: remove compiled d.ts from repo

--- packages/classname/classname.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type NoStrictEntityMods = Record<string, string | boolean | number | undefined>;
-export declare type ClassNameInitilizer = (blockName: string, elemName?: string) => ClassNameFormatter;
-export declare type ClassNameList = Array<string | undefined>;
-export declare type ClassNameFormatter = (elemNameOrBlockMods?: NoStrictEntityMods | string | null, elemModsOrBlockMix?: NoStrictEntityMods | ClassNameList | null, elemMix?: ClassNameList) => string;
-export declare function withNaming(preset: any): ClassNameInitilizer;
-export declare const cn: ClassNameInitilizer;
-export declare const classnames: (...strings: (string | undefined)[]) => string;
