e45bf1a5a28db654b4cece32fc5d70ef1f90d9bb
yarastqt <yarastqt@gmail.com>
2019-05-23 12:45:00 +0300

    feat(core): add composeU for union types

--- packages/core/core.tsx
+++ packages/core/core.tsx
@@ -151,3 +151,34 @@ export function compose(...fns: Array<HOC<any>>): Composition<any>;
 export function compose(...funcs: any[]) {
     return funcs.reduce((a, b) => (...args: any[]) => a(b(...args)), (arg: any) => arg);
 }
+
+/* tslint:disable:max-line-length */
+export function composeU<T1>(fn1: HOC<T1>): Composition<T1>;
+
+export function composeU<T1, T2>(fn1: HOC<T1>, fn2: HOC<T2>):
+    Composition<T1 | T2>;
+
+export function composeU<T1, T2, T3>(fn1: HOC<T1>, fn2: HOC<T2>, fn3: HOC<T3>):
+    Composition<T1 | T2 | T3>;
+
+export function composeU<T1, T2, T3, T4>(fn1: HOC<T1>, fn2: HOC<T2>, fn3: HOC<T3>, fn4: HOC<T4>):
+    Composition<T1 | T2 | T3 | T4>;
+
+export function composeU<T1, T2, T3, T4, T5>(fn1: HOC<T1>, fn2: HOC<T2>, fn3: HOC<T3>, fn4: HOC<T4>, fn5: HOC<T5>):
+    Composition<T1 | T2 | T3 | T4 | T5>;
+
+export function composeU<T1, T2, T3, T4, T5, T6>(fn1: HOC<T1>, fn2: HOC<T2>, fn3: HOC<T3>, fn4: HOC<T4>, fn5: HOC<T5>, fn6: HOC<T6>):
+    Composition<T1 | T2 | T3 | T4 | T5 | T6>;
+
+export function composeU<T1, T2, T3, T4, T5, T6, T7>(fn1: HOC<T1>, fn2: HOC<T2>, fn3: HOC<T3>, fn4: HOC<T4>, fn5: HOC<T5>, fn6: HOC<T6>, fn7: HOC<T7>):
+    Composition<T1 | T2 | T3 | T4 | T5 | T6 | T7>;
+
+export function composeU<T1, T2, T3, T4, T5, T6, T7, T8>(fn1: HOC<T1>, fn2: HOC<T2>, fn3: HOC<T3>, fn4: HOC<T4>, fn5: HOC<T5>, fn6: HOC<T6>, fn7: HOC<T7>, fn8: HOC<T8>):
+    Composition<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8>;
+
+export function composeU(...fns: Array<HOC<any>>): Composition<any>;
+/* tslint:enable:max-line-length */
+
+export function composeU(...args: any[]) {
+    return compose(...args);
+}
