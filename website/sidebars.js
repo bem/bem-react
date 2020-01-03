b7497eda7a5ccafaf197c8652066caef4d4f2536
yarastqt <yarastqt@gmail.com>
2020-01-03 21:35:43 +0300

    chore(website): add links to sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,7 +6,14 @@ module.exports = {
   api: {
     '@bem-react/core': ['api/core/core', 'api/core/compose'],
     '@bem-react/di': ['api/di/di'],
-    '@bem-react/classname': ['api/classname/classname'],
+    '@bem-react/classname': [
+      'api/classname/classname',
+      'api/classname/cn',
+      'api/classname/withNaming',
+      'api/classname/Preset',
+      'api/classname/ClassNameInitilizer',
+      'api/classname/ClassNameFormatter',
+    ],
     '@bem-react/classnames': ['api/classnames/classnames'],
   },
 }
