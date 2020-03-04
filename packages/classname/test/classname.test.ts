166d0fae45781be1a058e4d74467be1f99828a0e
yarastqt <yarastqt@gmail.com>
2020-03-04 11:51:26 +0300

    chore: use jest format for unit

--- packages/classname/test/classname.test.ts
+++ packages/classname/test/classname.test.ts
@@ -1,133 +1,130 @@
-import { describe, it } from 'mocha'
-import { expect } from 'chai'
-
 import { cn, withNaming } from '../classname'
 
 describe('@bem-react/classname', () => {
   describe('cn', () => {
-    it('block', () => {
+    test('block', () => {
       const b = cn('Block')
-      expect(b()).to.be.eq('Block')
+      expect(b()).toEqual('Block')
     })
 
-    it('elem', () => {
+    test('elem', () => {
       const e = cn('Block', 'Elem')
-      expect(e()).to.be.eq('Block-Elem')
+      expect(e()).toEqual('Block-Elem')
     })
 
     describe('modifiers', () => {
-      it('block', () => {
+      test('block', () => {
         const b = cn('Block')
-        expect(b({ modName: true })).to.be.eq('Block Block_modName')
+        expect(b({ modName: true })).toEqual('Block Block_modName')
       })
 
-      it('elem', () => {
+      test('elem', () => {
         const e = cn('Block', 'Elem')
-        expect(e({ modName: true })).to.be.eq('Block-Elem Block-Elem_modName')
+        expect(e({ modName: true })).toEqual('Block-Elem Block-Elem_modName')
       })
 
-      it('more than one', () => {
+      test('more than one', () => {
         const mods = { modName: true, modName2: 'modVal' }
         const b = cn('Block')
         const e = cn('Block', 'Elem')
 
-        expect(b(mods)).to.be.eq('Block Block_modName Block_modName2_modVal')
-        expect(e(mods)).to.be.eq('Block-Elem Block-Elem_modName Block-Elem_modName2_modVal')
+        expect(b(mods)).toEqual('Block Block_modName Block_modName2_modVal')
+        expect(e(mods)).toEqual('Block-Elem Block-Elem_modName Block-Elem_modName2_modVal')
       })
 
-      it('empty', () => {
+      test('empty', () => {
         const b = cn('Block')
-        expect(b({})).to.be.eq('Block')
+        expect(b({})).toEqual('Block')
       })
 
-      it('falsy', () => {
+      test('falsy', () => {
         const b = cn('Block')
-        expect(b({ modName: false })).to.be.eq('Block')
+        expect(b({ modName: false })).toEqual('Block')
       })
 
-      it('with falsy', () => {
+      test('with falsy', () => {
         const b = cn('Block', 'Elem')
-        expect(b({ modName: false, mod: 'val' })).to.be.eq('Block-Elem Block-Elem_mod_val')
+        expect(b({ modName: false, mod: 'val' })).toEqual('Block-Elem Block-Elem_mod_val')
       })
 
-      it('zero', () => {
+      test('zero', () => {
         const b = cn('Block')
-        expect(b({ modName: '0' })).to.be.eq('Block Block_modName_0')
+        expect(b({ modName: '0' })).toEqual('Block Block_modName_0')
       })
 
-      it('undefined', () => {
+      test('undefined', () => {
         const b = cn('Block')
-        expect(b({ modName: undefined })).to.be.eq('Block')
+        expect(b({ modName: undefined })).toEqual('Block')
       })
     })
 
     describe('mix', () => {
-      it('block', () => {
+      test('block', () => {
         const b = cn('Block')
-        expect(b(null, ['Mix1', 'Mix2'])).to.be.eq('Block Mix1 Mix2')
+        expect(b(null, ['Mix1', 'Mix2'])).toEqual('Block Mix1 Mix2')
       })
 
-      it('block with mods', () => {
+      test('block with mods', () => {
         const b = cn('Block')
-        expect(b({ theme: 'normal' }, ['Mix'])).to.be.eq('Block Block_theme_normal Mix')
+        expect(b({ theme: 'normal' }, ['Mix'])).toEqual('Block Block_theme_normal Mix')
       })
 
-      it('elem', () => {
+      test('elem', () => {
         const e = cn('Block', 'Elem')
-        expect(e(null, ['Mix1', 'Mix2'])).to.be.eq('Block-Elem Mix1 Mix2')
+        expect(e(null, ['Mix1', 'Mix2'])).toEqual('Block-Elem Mix1 Mix2')
       })
 
-      it('elem with mods', () => {
+      test('elem with mods', () => {
         const e = cn('Block', 'Elem')
-        expect(e({ theme: 'normal' }, ['Mix'])).to.be.eq('Block-Elem Block-Elem_theme_normal Mix')
+        expect(e({ theme: 'normal' }, ['Mix'])).toEqual('Block-Elem Block-Elem_theme_normal Mix')
       })
 
-      it('carry elem', () => {
+      test('carry elem', () => {
         const b = cn('Block')
-        expect(b('Elem', ['Mix1', 'Mix2'])).to.be.eq('Block-Elem Mix1 Mix2')
+        expect(b('Elem', ['Mix1', 'Mix2'])).toEqual('Block-Elem Mix1 Mix2')
       })
 
-      it('carry elem with mods', () => {
+      test('carry elem with mods', () => {
         const b = cn('Block')
-        expect(b('Elem', { theme: 'normal' }, ['Mix'])).to.be.eq(
+        expect(b('Elem', { theme: 'normal' }, ['Mix'])).toEqual(
           'Block-Elem Block-Elem_theme_normal Mix',
         )
       })
 
-      it('undefined', () => {
+      test('undefined', () => {
         const b = cn('Block')
-        expect(b('Elem', null, [undefined])).to.be.eq('Block-Elem')
+        expect(b('Elem', null, [undefined])).toEqual('Block-Elem')
       })
 
       /** @see https://github.com/bem/bem-react/issues/445 */
-      it('not string and not undefined', () => {
+      test('not string and not undefined', () => {
         const b = cn('Block')
-        expect(b('Elem', null, [false as any])).to.be.eq('Block-Elem')
-        expect(b('Elem', null, [true as any])).to.be.eq('Block-Elem')
-        expect(b('Elem', null, [10 as any])).to.be.eq('Block-Elem')
-        expect(b('Elem', null, [null as any])).to.be.eq('Block-Elem')
+        expect(b('Elem', null, [false as any])).toEqual('Block-Elem')
+        expect(b('Elem', null, [true as any])).toEqual('Block-Elem')
+        expect(b('Elem', null, [10 as any])).toEqual('Block-Elem')
+        expect(b('Elem', null, [null as any])).toEqual('Block-Elem')
       })
 
-      it('unique block', () => {
+      test('unique block', () => {
         const b = cn('Block')
-        expect(b(null, ['Block'])).to.be.eq('Block')
+        expect(b(null, ['Block'])).toEqual('Block')
       })
 
-      it('unique block with mods', () => {
+      test('unique block with mods', () => {
         const b = cn('Block')
-        expect(b({ theme: 'normal' }, ['Block Block_size_m'])).to.be.eq(
+        expect(b({ theme: 'normal' }, ['Block Block_size_m'])).toEqual(
           'Block Block_theme_normal Block_size_m',
         )
       })
 
-      it('unique elem', () => {
+      test('unique elem', () => {
         const b = cn('Block')
-        expect(b('Elem', null, ['Block-Elem'])).to.be.eq('Block-Elem')
+        expect(b('Elem', null, ['Block-Elem'])).toEqual('Block-Elem')
       })
 
-      it('unique elem with mods', () => {
+      test('unique elem with mods', () => {
         const b = cn('Block')
-        expect(b('Elem', { theme: 'normal' }, ['Block-Elem Block-Elem_size_m'])).to.be.eq(
+        expect(b('Elem', { theme: 'normal' }, ['Block-Elem Block-Elem_size_m'])).toEqual(
           'Block-Elem Block-Elem_theme_normal Block-Elem_size_m',
         )
       })
@@ -140,54 +137,54 @@ describe('@bem-react/classname', () => {
       m: '_',
     })
 
-    it('block', () => {
+    test('block', () => {
       const b = cCn('block')
-      expect(b()).to.be.eq('block')
+      expect(b()).toEqual('block')
     })
 
-    it('elem', () => {
+    test('elem', () => {
       const e = cCn('block', 'elem')
-      expect(e()).to.be.eq('block__elem')
+      expect(e()).toEqual('block__elem')
     })
 
     describe('modifiers', () => {
-      it('block', () => {
+      test('block', () => {
         const b = cCn('block')
-        expect(b({ modName: true })).to.be.eq('block block_modName')
+        expect(b({ modName: true })).toEqual('block block_modName')
       })
 
-      it('elem', () => {
+      test('elem', () => {
         const e = cCn('block', 'elem')
-        expect(e({ modName: true })).to.be.eq('block__elem block__elem_modName')
+        expect(e({ modName: true })).toEqual('block__elem block__elem_modName')
       })
 
-      it('more than one', () => {
+      test('more than one', () => {
         const mods = { modName: true, modName2: 'modVal' }
         const b = cCn('block')
         const e = cCn('block', 'elem')
 
-        expect(b(mods)).to.be.eq('block block_modName block_modName2_modVal')
-        expect(e(mods)).to.be.eq('block__elem block__elem_modName block__elem_modName2_modVal')
+        expect(b(mods)).toEqual('block block_modName block_modName2_modVal')
+        expect(e(mods)).toEqual('block__elem block__elem_modName block__elem_modName2_modVal')
       })
 
-      it('empty', () => {
+      test('empty', () => {
         const b = cCn('block')
-        expect(b({})).to.be.eq('block')
+        expect(b({})).toEqual('block')
       })
 
-      it('falsy', () => {
+      test('falsy', () => {
         const b = cCn('block')
-        expect(b({ modName: false })).to.be.eq('block')
+        expect(b({ modName: false })).toEqual('block')
       })
 
-      it('with falsy', () => {
+      test('with falsy', () => {
         const b = cCn('block')
-        expect(b({ modName: false, mod: 'val' })).to.be.eq('block block_mod_val')
+        expect(b({ modName: false, mod: 'val' })).toEqual('block block_mod_val')
       })
 
-      it('zero', () => {
+      test('zero', () => {
         const b = cCn('block')
-        expect(b({ modName: '0' })).to.be.eq('block block_modName_0')
+        expect(b({ modName: '0' })).toEqual('block block_modName_0')
       })
     })
   })
@@ -199,34 +196,32 @@ describe('@bem-react/classname', () => {
       v: '_',
     })
 
-    it('variants', () => {
+    test('variants', () => {
       const block = customCn('block')
 
-      expect(block({ mod: true })).to.be.equal('block block--mod')
-      expect(block({ mod: false })).to.be.equal('block')
-      expect(block({ mod: 'value' })).to.be.equal('block block--mod_value')
-      expect(block('element', { mod: true })).to.be.equal('block__element block__element--mod')
-      expect(block('element', { mod: false })).to.be.equal('block__element')
-      expect(block('element', { mod: 'value' })).to.be.equal(
-        'block__element block__element--mod_value',
-      )
+      expect(block({ mod: true })).toEqual('block block--mod')
+      expect(block({ mod: false })).toEqual('block')
+      expect(block({ mod: 'value' })).toEqual('block block--mod_value')
+      expect(block('element', { mod: true })).toEqual('block__element block__element--mod')
+      expect(block('element', { mod: false })).toEqual('block__element')
+      expect(block('element', { mod: 'value' })).toEqual('block__element block__element--mod_value')
     })
   })
 
   describe('carry', () => {
-    it('alone', () => {
+    test('alone', () => {
       const e = cn('Block')
-      expect(e('Elem')).to.be.eq('Block-Elem')
+      expect(e('Elem')).toEqual('Block-Elem')
     })
 
-    it('with mods', () => {
+    test('with mods', () => {
       const e = cn('Block')
-      expect(e('Elem', { modName: true })).to.be.eq('Block-Elem Block-Elem_modName')
+      expect(e('Elem', { modName: true })).toEqual('Block-Elem Block-Elem_modName')
     })
 
-    it('with elemMods', () => {
+    test('with elemMods', () => {
       const e = cn('Block', 'Elem')
-      expect(e({ modName: true })).to.be.eq('Block-Elem Block-Elem_modName')
+      expect(e({ modName: true })).toEqual('Block-Elem Block-Elem_modName')
     })
   })
 })
--- packages/classnames/test/classnames.test.ts
+++ packages/classnames/test/classnames.test.ts
@@ -1,20 +1,17 @@
-import { describe, it } from 'mocha'
-import { expect } from 'chai'
-
 import { classnames } from '../classnames'
 
 describe('@bem-react/classnames', () => {
   describe('classnames', () => {
-    it('empty', () => {
-      expect(classnames()).to.be.eq('')
+    test('empty', () => {
+      expect(classnames()).toEqual('')
     })
 
-    it('undefined', () => {
-      expect(classnames('Block', undefined, 'Block2')).to.be.eq('Block Block2')
+    test('undefined', () => {
+      expect(classnames('Block', undefined, 'Block2')).toEqual('Block Block2')
     })
 
-    it('uniq', () => {
-      expect(classnames('CompositeBlock', 'Block', 'Test', 'Block')).to.be.eq(
+    test('uniq', () => {
+      expect(classnames('CompositeBlock', 'Block', 'Test', 'Block')).toEqual(
         'CompositeBlock Block Test',
       )
     })
