33859c4f59b3cf9a2b2474ab1c33fd845fadc616
yarastqt <yarastqt@gmail.com>
2020-07-22 01:05:12 +0300

    fix: use output as fallback forr to

--- packages/pack/plugins/CopyAssetsPlugin.ts
+++ packages/pack/plugins/CopyAssetsPlugin.ts
@@ -4,20 +4,20 @@ import glob from 'fast-glob'
 
 import { Plugin, OnDone, Payload } from '../interfaces'
 
-type rules = Array<{
+type Rules = Array<{
   from: string
-  to: string
+  to?: string
   transformPath?: (path: string) => string
 }>
 
 export class CopyAssetsPlugin implements Plugin {
-  constructor(public rules: rules) {}
+  constructor(public rules: Rules) {}
 
-  async onAfterRun(done: OnDone, { context }: Payload) {
+  async onAfterRun(done: OnDone, { context, output }: Payload) {
     for (const rule of this.rules) {
       const files = await glob(rule.from, { cwd: context })
       for (const file of files) {
-        let destPath = resolve(context, rule.to, file)
+        let destPath = resolve(context, rule.to || output, file)
         if (rule.transformPath !== undefined) {
           destPath = rule.transformPath(destPath)
         }
