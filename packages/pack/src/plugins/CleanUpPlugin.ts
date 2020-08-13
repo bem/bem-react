a96890b19abc15d3bce58ac9fa39d10febd9a1ed
yarastqt <yarastqt@gmail.com>
2020-08-13 19:04:50 +0300

    feat: add CleanUpPlugin

---
+++ packages/pack/src/plugins/CleanUpPlugin.ts
@@ -0,0 +1,17 @@
+import { remove } from 'fs-extra'
+import { OnDone, Plugin } from '../interfaces'
+
+class CleanUpPlugin implements Plugin {
+  constructor(public sources: string[]) {}
+
+  async onBeforeRun(done: OnDone) {
+    for (const source of this.sources) {
+      await remove(source)
+    }
+    done()
+  }
+}
+
+export function useCleanUpPlugin(sources: string[]): CleanUpPlugin {
+  return new CleanUpPlugin(sources)
+}
