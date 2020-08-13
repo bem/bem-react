d96fcfbb452e927d280e11c4ad094b37060c7c26
yarastqt <yarastqt@gmail.com>
2020-08-13 16:14:46 +0300

    chore: remove from sources from root

--- packages/pack/wrapToPromise.ts
+++
@@ -1,8 +0,0 @@
-export function wrapToPromise(
-  fn: (resolve: any, payload: any) => void,
-  payload: any,
-): Promise<void> {
-  return new Promise((resolve) => {
-    fn(resolve, payload)
-  })
-}
