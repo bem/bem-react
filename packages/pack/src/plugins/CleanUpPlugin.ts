d3030cb7c8a2420f774a7113d96f4ed3ccb13fdd
yarastqt <yarastqt@gmail.com>
2020-08-16 02:35:27 +0300

    feat: add debug marks for CleanUpPlugin

--- packages/pack/src/plugins/CleanUpPlugin.ts
+++ packages/pack/src/plugins/CleanUpPlugin.ts
@@ -1,13 +1,18 @@
 import { remove } from 'fs-extra'
 import { OnDone, Plugin } from '../interfaces'
+import { mark } from '../debug'
 
 class CleanUpPlugin implements Plugin {
-  constructor(public sources: string[]) {}
+  constructor(public sources: string[]) {
+    mark('CleanUpPlugin::constructor')
+  }
 
   async onBeforeRun(done: OnDone) {
+    mark('CleanUpPlugin::onAfterRun(start)')
     for (const source of this.sources) {
       await remove(source)
     }
+    mark('CleanUpPlugin::onAfterRun(finish)')
     done()
   }
 }
