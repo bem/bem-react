9c401e1b6770ab6591e12fb31dc6ca839b98abff
yarastqt <yarastqt@gmail.com>
2020-08-16 02:33:20 +0300

    feat: add context for CopyAssetsPlugin

--- packages/pack/src/plugins/CopyAssetsPlugin.ts
+++ packages/pack/src/plugins/CopyAssetsPlugin.ts
@@ -6,6 +6,7 @@ import { Plugin, OnDone, Payload } from '../interfaces'
 type Rules = Array<{
   from: string
   to?: string
+  context?: string
   ignore?: string[]
   transformPath?: (path: string) => string
 }>
@@ -15,14 +16,15 @@ export class CopyAssetsPlugin implements Plugin {
 
   async onAfterRun(done: OnDone, { context, output }: Payload) {
     for (const rule of this.rules) {
-      const files = await glob(rule.from, { cwd: context, ignore: rule.ignore })
+      const ctx = rule.context ? resolve(context, rule.context) : context
+      const files = await glob(rule.from, { cwd: ctx, ignore: rule.ignore })
       for (const file of files) {
-        let destPath = resolve(context, rule.to || output, file)
+        let destPath = resolve(ctx, rule.to || output, file)
         if (rule.transformPath !== undefined) {
           destPath = rule.transformPath(destPath)
         }
         await ensureDir(dirname(destPath))
-        await copyFile(resolve(context, file), destPath)
+        await copyFile(resolve(ctx, file), destPath)
       }
     }
     done()
