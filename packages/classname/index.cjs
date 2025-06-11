7ef51f8a96d1b81f60458a4772237813eb630c42
yarastqt <yarastqt@gmail.com>
2025-06-11 12:20:58 +0300

    feat: support esm modules

--- packages/classname/index.cjs
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./build/classname.production.min.cjs')
-} else {
-  module.exports = require('./build/classname.development.cjs')
-}
